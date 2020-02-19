d9e22e85a8dae459bbcdcf1fc2fe1fdfea12b958
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-02-19 19:50:33 +0300

    0.0.2

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Привет')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай')
+
+
 bot.polling()
