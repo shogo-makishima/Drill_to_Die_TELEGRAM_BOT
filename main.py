fc446895ed87383651ce7733611e5c8a3d6ac1ea
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-02-20 21:41:30 +0300

    0.0.3f5

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import os, telebot
-from flask import Flask, request
 from Classes.Main import Main
 
 Main.Start(Main)
 
 TOKEN = '1006756726:AAEjnh_9yROdhIss825lDjrizRXC1B7th6I'
 bot = telebot.TeleBot(TOKEN)
-
-server = Flask(__name__)
-
 
 
 @bot.message_handler(commands=['start'])
@@ -24,20 +20,5 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+bot.polling()
 
