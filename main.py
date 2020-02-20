fce403726460d0b1cd0e207aac27ef6bed0ed9ae
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-02-20 21:59:01 +0300

    0.0.3f9

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, telebot
+import os, telebot, logging
 from flask import Flask, request
 from Classes.Main import Main
 
@@ -6,7 +6,6 @@ Main.Start(Main)
 
 TOKEN = '1006756726:AAEjnh_9yROdhIss825lDjrizRXC1B7th6I'
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
@@ -17,18 +16,25 @@ def start(message):
 def start_message(message):
     bot.send_message(message.chat.id, Main.GetItemsString(Main))
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
 
+if ("HEROKU" in list(os.environ.keys())):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
 
-@server.route("/")
-def webhook():
+    server = Flask(__name__)
+
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://drill-to-die-bot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    bot.polling(none_stop=True)
