24e84accd66cd8feed561019b1e78de8e2a3f258
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-02-20 21:24:16 +0300

    0.0.3f3

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-import telebot
+import os, telebot
+from flask import Flask, request
 from Classes.Main import Main
 
 Main.Start(Main)
 
-bot = telebot.TeleBot('1006756726:AAEjnh_9yROdhIss825lDjrizRXC1B7th6I')
+TOKEN = '1006756726:AAEjnh_9yROdhIss825lDjrizRXC1B7th6I'
+bot = telebot.TeleBot(TOKEN)
 
+server = Flask(__name__)
+
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 @bot.message_handler(commands=['materials'])
 def start_message(message):
     bot.send_message(message.chat.id, Main.GetItemsString(Main))
 
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if (message.text == 'Привет'):
-        bot.send_message(message.chat.id, "Привет")
-    elif (message.text == 'Пока'):
-        bot.send_message(message.chat.id, 'Прощай')
-    else:
-        bot.send_message(message.chat.id, 'Я тебя не понимаю')
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-bot.polling()
 
-# print(Main.GetItemsString(Main))
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
