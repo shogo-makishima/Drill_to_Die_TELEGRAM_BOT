f5db3935066cc2add56f11d777578a903e4327db
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-02-20 22:01:58 +0300

    0.0.3f10

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ if ("HEROKU" in list(os.environ.keys())):
     logger = telebot.logger
     telebot.logger.setLevel(logging.INFO)
 
+    bot.remove_webhook()
+    bot.set_webhook(url="https://drill-to-die-bot.herokuapp.com/bot")  # этот url нужно заменить на url вашего Хероку приложения
+
     server = Flask(__name__)
 
     @server.route("/bot", methods=['POST'])
@@ -28,11 +31,11 @@ if ("HEROKU" in list(os.environ.keys())):
         bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
         return "!", 200
 
-    @server.route("/")
+    """"@server.route("/")
     def webhook():
         bot.remove_webhook()
         bot.set_webhook(url="https://drill-to-die-bot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
+        return "?", 200"""
 
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:
